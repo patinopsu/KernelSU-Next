20edfb9f32a2879841dabf6a0f0dc45ba3bb1911
Patin Muangjan <patin@patin.dev>
2025-10-17 16:10:46 +0700

    Modify setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -39,11 +39,11 @@ perform_cleanup() {
 # Sets up or update KernelSU-Next environment
 setup_kernelsu() {
     echo "[+] Setting up KernelSU-Next..."
-    test -d "$GKI_ROOT/KernelSU-Next" || git clone https://github.com/KernelSU-Next/KernelSU-Next && echo "[+] Repository cloned."
+    test -d "$GKI_ROOT/KernelSU-Next" || git submodule add https://github.com/patinopsu/KernelSU-Next.git -b susfs-patch && echo "[+] Repository cloned."
     cd "$GKI_ROOT/KernelSU-Next"
     git stash && echo "[-] Stashed current changes."
     if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
-        git checkout next && echo "[-] Switched to next branch."
+        echo [+-] Doing Nothing
     fi
     git pull && echo "[+] Repository updated."
     if [ -z "${1-}" ]; then
